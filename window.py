8b2b7f8c73b81e0337edce24a9c7e047dc42965d
jeronimovelasqz <103902591+jeronimovelasqz@users.noreply.github.com>
2022-12-25 13:29:09 -0500

    Update window.py

--- window.py
+++ window.py
@@ -1,67 +1,70 @@
 from tkinter import *
 
+class Menu1Apoo:
 
-def btn_clicked():
-    print("Button Clicked")
+    def __init__(self):
+        self.window = Tk()
+
+        self.window.geometry("1136x951")
+        self.window.configure(bg = "#ffffff")
+        self.canvas = Canvas(
+            self.window,
+            bg = "#ffffff",
+            height = 951,
+            width = 1136,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge")
+        self.canvas.place(x = 0, y = 0)
+
+        self.background_img = PhotoImage(file = f"background.png")
+        self.background = self.canvas.create_image(
+            743.5, 515.5,
+            image=self.background_img)
+
+        self.img0 = PhotoImage(file = f"img0.png")
+        self.b0 = Button(
+            image = self.img0,
+            borderwidth = 0,
+            highlightthickness = 0,
+            command = self.btn_clicked,
+            relief = "flat")
+
+        self.b0.place(
+            x = 632, y = 500,
+            width = 370,
+            height = 51)
+
+        self.img1 = PhotoImage(file = f"img1.png")
+        self.b1 = Button(
+            image = self.img1,
+            borderwidth = 0,
+            highlightthickness = 0,
+            command = self.btn_clicked,
+            relief = "flat")
+
+        self.b1.place(
+            x = 648, y = 579,
+            width = 397,
+            height = 48)
+
+        self.img2 = PhotoImage(file = f"img2.png")
+        self.b2 = Button(
+            image = self.img2,
+            borderwidth = 0,
+            highlightthickness = 0,
+            command = self.btn_clicked,
+            relief = "flat")
+
+        self.b2.place(
+            x = 657, y = 649,
+            width = 322,
+            height = 48)
+
+        self.window.resizable(False, False)
+        self.window.mainloop()
+    def btn_clicked(self):
+        print("Button Clicked")
 
 
-window = Tk()
-
-window.geometry("1136x951")
-window.configure(bg = "#ffffff")
-canvas = Canvas(
-    window,
-    bg = "#ffffff",
-    height = 951,
-    width = 1136,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge")
-canvas.place(x = 0, y = 0)
-
-background_img = PhotoImage(file = f"background.png")
-background = canvas.create_image(
-    743.5, 515.5,
-    image=background_img)
-
-img0 = PhotoImage(file = f"img0.png")
-b0 = Button(
-    image = img0,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
-
-b0.place(
-    x = 632, y = 500,
-    width = 370,
-    height = 51)
-
-img1 = PhotoImage(file = f"img1.png")
-b1 = Button(
-    image = img1,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
-
-b1.place(
-    x = 648, y = 579,
-    width = 397,
-    height = 48)
-
-img2 = PhotoImage(file = f"img2.png")
-b2 = Button(
-    image = img2,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
-
-b2.place(
-    x = 657, y = 649,
-    width = 322,
-    height = 48)
-
-window.resizable(False, False)
-window.mainloop()
+eso = Menu1Apoo()
